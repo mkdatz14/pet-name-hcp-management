eb4bd4c61a4f50126bfd3d54464f6d77082001dd
Michael Datz <michael.kennedydatz@hashicorp.com>
2026-06-10 11:01:28 -0400

    fix reference to oauth token

--- workspaces.tf
+++ workspaces.tf
@@ -27,7 +27,7 @@ resource "tfe_workspace" "pet_name" {
 
     vcs_repo {
         identifier = "mkdatz/terraform-random-pet-name"
-        oauth_token_id = data.tfe_outputs.hcp_management.outputs.github_token_id
+        oauth_token_id = data.tfe_outputs.hcp_management.values.github_token_id
         branch = "main"
     }
 
