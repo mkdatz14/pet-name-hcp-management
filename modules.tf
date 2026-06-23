01e370a0c80a0b7f90fba4121567c652496fe815
Michael Datz <michael.kennedydatz@hashicorp.com>
2026-06-23 11:32:18 -0400

    make branch empty

--- modules.tf
+++ modules.tf
@@ -6,7 +6,6 @@ resource "tfe_registry_module" "pet_name" {
     identifier                 = "mkdatz14/terraform-random-pet-name"
     github_app_installation_id = local.github_app_installation_id
     oauth_token_id             = local.github_app_installation_id == null ? local.github_oauth_token_id : null
-    branch                     = "main"
     tag_prefix                 = "v"
   }
 }
