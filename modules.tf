5525b0361ead05d581aba836cdafa3314b0b763f
Michael Datz <michael.kennedydatz@hashicorp.com>
2026-06-23 11:40:59 -0400

    change the oauth method

--- modules.tf
+++ modules.tf
@@ -2,10 +2,11 @@ resource "tfe_registry_module" "pet_name" {
   organization = local.organization
 
   vcs_repo {
-    display_identifier         = "mkdatz14/terraform-random-pet-name"
-    identifier                 = "mkdatz14/terraform-random-pet-name"
-    github_app_installation_id = local.github_app_installation_id
-    oauth_token_id             = local.github_app_installation_id == null ? local.github_oauth_token_id : null
+    display_identifier = "mkdatz14/terraform-random-pet-name"
+    identifier         = "mkdatz14/terraform-random-pet-name"
+    # Use org OAuth for module registry access; this repo may not be installed in the GitHub App.
+    github_app_installation_id = null
+    oauth_token_id             = local.github_oauth_token_id
     tag_prefix                 = "v"
   }
 }
